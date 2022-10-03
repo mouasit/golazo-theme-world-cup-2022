4b0c4eafb2b8bb2c265a93ed38bed90adb23e70d
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-03 18:11:50 +0100

    create env add component of Live

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './Style/App.css';
+import Live from './Components/Live';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <Live />
+  )
 }
 
 export default App;
