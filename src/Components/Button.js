2c266531d947a8aa3aebf98a8e1a4021f55b42e0
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-05 01:16:11 +0100

    add language button and fix height issues

---
+++ src/Components/Button.js
@@ -0,0 +1,9 @@
+const Button = () => (
+<svg xmlns="http://www.w3.org/2000/svg" width="259.205" height="48.673" viewBox="0 0 259.205 48.673">
+  <path id="Path_1613" data-name="Path 1613" d="M382.231,766.069H615.209l9.114,24.336-9.114,24.336H382.231l-9.114-24.336Z" transform="translate(-365.118 -766.069)" fill="#eeeee4"/>
+  <rect id="Rectangle_105" data-name="Rectangle 105" width="15.397" height="16.191" transform="matrix(-0.707, 0.707, -0.707, -0.707, 22.337, 24.26)" fill="#42bfac"/>
+</svg>
+
+)
+
+export default Button;
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,10 @@
 import logo from "../assets/logo.svg"
 import Background from "./Background"
+import Button from "./Button"
+import englishFlag from "../assets/flags/languages/english.svg"
+import { BiChevronRight } from "react-icons/bi";
+
+
 const Nav = () =>{
     return(
         <nav className="side-nav">
@@ -32,7 +37,19 @@ const Nav = () =>{
                 </a>
             </li>
         </ul>
-        <button>Language</button>
+        <button>
+            <Button />
+            <span className="name-flag">
+                <img src={englishFlag} alt="" />
+                <span>English</span>
+            </span>
+            <span className="arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="21.293" height="24.286" viewBox="0 0 21.293 24.286">
+                    <path id="Path_1738" data-name="Path 1738" d="M793.082,1047.652,782.435,1059.8l10.647,12.143,10.646-12.143Z" transform="translate(-782.435 -1047.652)" fill="#47b7ab"/>
+                </svg>
+                <BiChevronRight />
+            </span>
+        </button>
     </nav>
     )
 }
