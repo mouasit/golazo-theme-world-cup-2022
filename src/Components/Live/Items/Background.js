cbd9914240a838212f1783eac3b08bc4dba96c16
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-09 03:18:17 +0100

    crate hover state

--- src/Components/Live/Items/Background.js
+++ src/Components/Live/Items/Background.js
@@ -1,7 +1,7 @@
 const Background = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="259.205" height="48.673" viewBox="0 0 259.205 48.673">
   <g id="Group_4536" data-name="Group 4536" transform="translate(-140 -419.388)">
-    <path id="Path_1611" data-name="Path 1611" d="M381.838,766.069h222.91l8.72,24.336-8.72,24.336H381.838l-8.72-24.336Z" transform="translate(-225.118 -346.681)" fill="#60071e"/>
+    <path id="Path_1611" data-name="Path 1611" d="M381.838,766.069h222.91l8.72,24.336-8.72,24.336H381.838l-8.72-24.336Z" transform="translate(-225.118 -346.681)"/>
     <rect id="Rectangle_101" data-name="Rectangle 101" width="15.397" height="16.191" transform="matrix(-0.707, 0.707, -0.707, -0.707, 162.337, 443.761)" fill="#42bfac"/>
     <rect id="Rectangle_102" data-name="Rectangle 102" width="15.397" height="16.191" transform="matrix(-0.707, 0.707, -0.707, -0.707, 399.205, 443.761)" fill="#42bfac"/>
   </g>
