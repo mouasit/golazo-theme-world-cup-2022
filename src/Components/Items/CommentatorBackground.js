f096263086cd020b984d4de5293c1c30ecb04c66
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-14 03:15:53 +0100

    info match

--- src/Components/Items/CommentatorBackground.js
+++ src/Components/Items/CommentatorBackground.js
@@ -1,17 +1,17 @@
 const CommentatorBackground = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="192.691" height="43.663" viewBox="0 0 192.691 43.663">
-  <g id="Group_4537" data-name="Group 4537" transform="translate(-1317.176 -256.388)">
-    <path id="Path_1772" data-name="Path 1772" d="M379.424,766.069H540.64l6.306,17.6-6.306,17.6H379.424l-6.306-17.6Z" transform="translate(962.922 -505.342)" fill="#e32052"/>
-    <rect id="Rectangle_150" data-name="Rectangle 150" width="29.966" height="31.783" transform="matrix(-0.707, 0.707, -0.707, -0.707, 1360.84, 278.862)" fill="#47b7ab"/>
-    <g id="Group_834" data-name="Group 834" transform="translate(1333.811 270.853)">
-      <g id="Group_48" data-name="Group 48" transform="translate(0 7.56)">
+<svg xmlns="http://www.w3.org/2000/svg" width="182.715" height="41.398" viewBox="0 0 182.715 41.398">
+  <g id="Group_4537" data-name="Group 4537" transform="translate(-1317.183 -256.391)">
+    <path id="Path_1772" data-name="Path 1772" d="M379.1,766.069H531.972l5.98,16.689-5.98,16.69H379.1l-5.98-16.69Z" transform="translate(961.946 -505.566)" fill="#e32052"/>
+    <rect id="Rectangle_150" data-name="Rectangle 150" width="28.415" height="30.138" transform="translate(1358.581 277.699) rotate(135)" fill="#47b7ab"/>
+    <g id="Group_834" data-name="Group 834" transform="translate(1332.947 270.105)">
+      <g id="Group_48" data-name="Group 48" transform="translate(0 7.169)">
         <g id="Group_47" data-name="Group 47">
-          <path id="Path_32" data-name="Path 32" d="M-73.665,76.212a.554.554,0,0,0-.554-.554.554.554,0,0,0-.554.554A3.921,3.921,0,0,1-78.7,80.133a3.921,3.921,0,0,1-3.921-3.921.554.554,0,0,0-.554-.554.554.554,0,0,0-.554.554,5.035,5.035,0,0,0,4.476,5v1.478h-2.015a.554.554,0,0,0-.554.554.554.554,0,0,0,.554.554h5.139a.554.554,0,0,0,.554-.554.554.554,0,0,0-.554-.554h-2.015V81.21A5.035,5.035,0,0,0-73.665,76.212Z" transform="translate(83.725 -75.658)" fill="#fff"/>
+          <path id="Path_32" data-name="Path 32" d="M-74.186,76.183a.525.525,0,0,0-.525-.525.525.525,0,0,0-.525.525,3.718,3.718,0,0,1-1.091,2.631A3.718,3.718,0,0,1-78.96,79.9a3.718,3.718,0,0,1-3.718-3.718.525.525,0,0,0-.525-.525.525.525,0,0,0-.525.525,4.774,4.774,0,0,0,4.244,4.741v1.4H-81.4a.525.525,0,0,0-.525.525.525.525,0,0,0,.525.525h4.873A.525.525,0,0,0-76,82.851a.525.525,0,0,0-.525-.525h-1.911v-1.4A4.774,4.774,0,0,0-74.186,76.183Z" transform="translate(83.729 -75.658)" fill="#fff"/>
         </g>
       </g>
-      <g id="Group_50" data-name="Group 50" transform="translate(1.939)">
+      <g id="Group_50" data-name="Group 50" transform="translate(1.842)">
         <g id="Group_49" data-name="Group 49">
-          <path id="Path_33" data-name="Path 33" d="M-17.381-170.915a3.09,3.09,0,0,0-3.09,3.09v5a3.094,3.094,0,0,0,3.09,3.09,3.094,3.094,0,0,0,3.09-3.09v-5A3.09,3.09,0,0,0-17.381-170.915Z" transform="translate(20.471 170.915)" fill="#fff"/>
+          <path id="Path_33" data-name="Path 33" d="M-17.541-170.915a2.93,2.93,0,0,0-2.93,2.93v4.741a2.934,2.934,0,0,0,2.93,2.93,2.934,2.934,0,0,0,2.93-2.93v-4.741A2.93,2.93,0,0,0-17.541-170.915Z" transform="translate(20.471 170.915)" fill="#fff"/>
         </g>
       </g>
     </g>
--- src/Components/Items/Location.js
+++ src/Components/Items/Location.js
@@ -1,9 +1,9 @@
 const LocatoinBackground = () => (
-<svg xmlns="http://www.w3.org/2000/svg" width="192.809" height="43.663" viewBox="0 0 192.809 43.663">
-  <g id="Group_4536" data-name="Group 4536" transform="translate(-687 -256.388)">
-    <path id="Path_1770" data-name="Path 1770" d="M379.424,766.069H540.64l6.306,17.6-6.306,17.6H379.424l-6.306-17.6Z" transform="translate(332.863 -505.342)" fill="#e32052"/>
-    <rect id="Rectangle_148" data-name="Rectangle 148" width="29.966" height="31.783" transform="matrix(-0.707, 0.707, -0.707, -0.707, 730.663, 278.862)" fill="#47b7ab"/>
-    <path id="Path_30" data-name="Path 30" d="M89.37,30.947a6.006,6.006,0,0,0-5.821,6.161c0,4.825,5.273,9.812,5.5,10.021a.469.469,0,0,0,.647,0c.224-.211,5.5-5.194,5.5-10.021A6.006,6.006,0,0,0,89.37,30.947Zm0,9.515a3.4,3.4,0,1,1,3.234-3.4A3.319,3.319,0,0,1,89.37,40.462Z" transform="translate(620.085 239.183)" fill="#fff"/>
+<svg xmlns="http://www.w3.org/2000/svg" width="182.826" height="41.398" viewBox="0 0 182.826 41.398">
+  <g id="Group_4536" data-name="Group 4536" transform="translate(-687.006 -256.391)">
+    <path id="Path_1770" data-name="Path 1770" d="M379.1,766.069H531.972l5.98,16.689-5.98,16.689H379.1l-5.98-16.689Z" transform="translate(331.881 -505.566)" fill="#e32052"/>
+    <rect id="Rectangle_148" data-name="Rectangle 148" width="28.415" height="30.138" transform="translate(728.404 277.699) rotate(135)" fill="#47b7ab"/>
+    <path id="Path_30" data-name="Path 30" d="M89.069,30.947a5.7,5.7,0,0,0-5.52,5.842c0,4.575,5,9.3,5.215,9.5a.445.445,0,0,0,.614,0c.212-.2,5.215-4.925,5.215-9.5a5.7,5.7,0,0,0-5.525-5.842Zm0,9.023a3.224,3.224,0,1,1,3.067-3.224A3.147,3.147,0,0,1,89.069,39.97Z" transform="translate(619.225 238.472)" fill="#fff"/>
   </g>
 </svg>
 
