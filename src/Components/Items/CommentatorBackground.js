55f34ac02637ede56c6ebed2f65c3cfebf169416
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-14 17:19:51 +0100

    alignement item info

--- src/Components/Items/CommentatorBackground.js
+++ src/Components/Items/CommentatorBackground.js
@@ -1,22 +1,23 @@
 const CommentatorBackground = () => (
-<svg xmlns="http://www.w3.org/2000/svg" width="182.715" height="41.398" viewBox="0 0 182.715 41.398">
-  <g id="Group_4537" data-name="Group 4537" transform="translate(-1317.183 -256.391)">
-    <path id="Path_1772" data-name="Path 1772" d="M379.1,766.069H531.972l5.98,16.689-5.98,16.69H379.1l-5.98-16.69Z" transform="translate(961.946 -505.566)" fill="#e32052"/>
-    <rect id="Rectangle_150" data-name="Rectangle 150" width="28.415" height="30.138" transform="translate(1358.581 277.699) rotate(135)" fill="#47b7ab"/>
-    <g id="Group_834" data-name="Group 834" transform="translate(1332.947 270.105)">
-      <g id="Group_48" data-name="Group 48" transform="translate(0 7.169)">
+<svg xmlns="http://www.w3.org/2000/svg" width="182.832" height="41.429" viewBox="0 0 182.832 41.429">
+  <g id="Group_4537" data-name="Group 4537" transform="translate(-1317.178 -256.388)">
+    <path id="Path_1772" data-name="Path 1772" d="M379.1,766.069H532.069l5.984,16.7-5.984,16.7H379.1l-5.984-16.7Z" transform="translate(961.957 -505.563)" fill="#e32052"/>
+    <rect id="Rectangle_150" data-name="Rectangle 150" width="28.432" height="30.156" transform="translate(1358.606 277.712) rotate(135)" fill="#47b7ab"/>
+    <g id="Group_834" data-name="Group 834" transform="translate(1332.958 270.113)">
+      <g id="Group_48" data-name="Group 48" transform="translate(0 7.172)">
         <g id="Group_47" data-name="Group 47">
-          <path id="Path_32" data-name="Path 32" d="M-74.186,76.183a.525.525,0,0,0-.525-.525.525.525,0,0,0-.525.525,3.718,3.718,0,0,1-1.091,2.631A3.718,3.718,0,0,1-78.96,79.9a3.718,3.718,0,0,1-3.718-3.718.525.525,0,0,0-.525-.525.525.525,0,0,0-.525.525,4.774,4.774,0,0,0,4.244,4.741v1.4H-81.4a.525.525,0,0,0-.525.525.525.525,0,0,0,.525.525h4.873A.525.525,0,0,0-76,82.851a.525.525,0,0,0-.525-.525h-1.911v-1.4A4.774,4.774,0,0,0-74.186,76.183Z" transform="translate(83.729 -75.658)" fill="#fff"/>
+          <path id="Path_32" data-name="Path 32" d="M-74.18,76.183a.525.525,0,0,0-.525-.525.525.525,0,0,0-.525.525,3.72,3.72,0,0,1-1.092,2.633A3.72,3.72,0,0,1-78.957,79.9a3.72,3.72,0,0,1-3.72-3.72.525.525,0,0,0-.525-.525.525.525,0,0,0-.525.525,4.777,4.777,0,0,0,4.247,4.744v1.4H-81.4a.525.525,0,0,0-.525.525.525.525,0,0,0,.525.525h4.876A.525.525,0,0,0-76,82.855a.525.525,0,0,0-.525-.525h-1.912v-1.4A4.777,4.777,0,0,0-74.18,76.183Z" transform="translate(83.728 -75.657)" fill="#fff"/>
         </g>
       </g>
       <g id="Group_50" data-name="Group 50" transform="translate(1.842)">
         <g id="Group_49" data-name="Group 49">
-          <path id="Path_33" data-name="Path 33" d="M-17.541-170.915a2.93,2.93,0,0,0-2.93,2.93v4.741a2.934,2.934,0,0,0,2.93,2.93,2.934,2.934,0,0,0,2.93-2.93v-4.741A2.93,2.93,0,0,0-17.541-170.915Z" transform="translate(20.471 170.915)" fill="#fff"/>
+          <path id="Path_33" data-name="Path 33" d="M-17.539-170.915a2.932,2.932,0,0,0-2.932,2.932v4.744a2.936,2.936,0,0,0,2.932,2.932,2.936,2.936,0,0,0,2.932-2.932v-4.744A2.932,2.932,0,0,0-17.539-170.915Z" transform="translate(20.471 170.915)" fill="#fff"/>
         </g>
       </g>
     </g>
   </g>
 </svg>
+
 )
 
 export default CommentatorBackground;
