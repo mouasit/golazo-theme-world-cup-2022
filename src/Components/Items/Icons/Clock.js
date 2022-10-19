71a645bd192379a3d9db0eb373f30594393b2525
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-19 03:33:49 +0100

    add time match

---
+++ src/Components/Items/Icons/Clock.js
@@ -0,0 +1,16 @@
+const Clock = () => (
+    <svg id="Group_603" data-name="Group 603" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 76.076 76.076">
+  <g id="Group_13" data-name="Group 13" transform="translate(35.092 17.64)">
+    <g id="Group_12" data-name="Group 12">
+      <path id="Path_27" data-name="Path 27" d="M-3.78,153.645l-10.605-7.953v-16.2a2.949,2.949,0,0,0-2.936-2.957,2.949,2.949,0,0,0-2.957,2.936v17.7a2.942,2.942,0,0,0,1.18,2.359l11.785,8.839a2.945,2.945,0,0,0,4.126-.592,2.946,2.946,0,0,0-.592-4.126Z" transform="translate(20.278 -126.532)" fill="#47b7ab"/>
+    </g>
+  </g>
+  <g id="Group_15" data-name="Group 15" transform="translate(0 0)">
+    <g id="Group_14" data-name="Group 14">
+      <path id="Path_28" data-name="Path 28" d="M11.066,123.167A38.038,38.038,0,1,0,49.1,161.205,38.041,38.041,0,0,0,11.066,123.167Zm0,70.179A32.146,32.146,0,1,1,43.212,161.2,32.143,32.143,0,0,1,11.066,193.346Z" transform="translate(26.972 -123.167)" fill="#47b7ab"/>
+    </g>
+  </g>
+</svg>
+)
+
+export default Clock;
--- src/Components/Live/Live.js
+++ src/Components/Live/Live.js
@@ -12,6 +12,8 @@ import flagSwitzerland from "../../assets/flags/teams/switzerland.svg";
 import flagSerbia from "../../assets/flags/teams/serbia.svg";
 import flagCameroon from "../../assets/flags/teams/cameroon.svg";
 import PlayIcon from "../Items/Icons/PlayIcon";
+import Vs from "../../Components/Items/Icons/Vs";
+import Clock from "../Items/Icons/Clock";
 
 const Live = () =>(
     <main>
@@ -51,100 +53,130 @@ const Live = () =>(
                 </div>
                 <div className="match">
                     <div className="match-info">
-                            <div className="item-info">
-                                <LocatoinBackground />
-                                <span className="name-item-info">Lusail</span>
+                        <div className="item-info">
+                            <LocatoinBackground />
+                            <span className="name-item-info">Lusail</span>
+                        </div>
+                        <div className="item-info">
+                            <ChanelBackground />
+                            <span className="name-item-info">Beinsport 05</span>
+                        </div>
+                        <div className="item-info">
+                            <CommentatorBackground />
+                            <span className="name-item-info">Hafid Derradji</span>
+                        </div>
+                    </div>
+                    <div className="match-time">
+                        <div className="teams">
+                            <div className="team">
+                                <img src={flagMorocco} alt="" />
+                                <span className="team-name">Morocco</span>
                             </div>
-                            <div className="item-info">
-                                <ChanelBackground />
-                                <span className="name-item-info">Beinsport 05</span>
-                            </div>
-                            <div className="item-info">
-                                <CommentatorBackground />
-                                <span className="name-item-info">Hafid Derradji</span>
+                            <span className="vs">
+                                <Vs />
+                            </span>
+                            <div className="team">
+                                <img src={flagMorocco} alt="" />
+                                <span className="team-name">manchister city</span>
                             </div>
                         </div>
-                        <div className="match-play">
-                            <span className="team">
-                                <img src={flagBelgium} alt="Flag Morocco" />
-                                <span className="team-name">Belgium</span>
-                            </span>
-                            <button className="play-button" aria-label="Play Button">
-                                <PlayIcon />
-                            </button>
-                            <span className="team team-right">
-                                <img src={flagCroatia} alt="Flag Morocco" />
-                                <span className="team-name">Croatia</span>
-                            </span>
+                        <div className="start-time">
+                            <Clock />
+                            <div className="time">
+                                <span>17</span>
+                                <span>:</span>
+                                <span>00</span>
+                            </div>
                         </div>
+                    </div>
                 </div>
             </div>
         </section>
         <section className="group-matches" aria-label="Group Matches">
             <div className="group-head">
-                <Group nameGroup="G" />
+                <Group nameGroup="F" />
             </div>
             <div className="all-matches">
                 <div className="match">
                     <div className="match-info">
                             <div className="item-info">
                                 <LocatoinBackground />
-                                <span className="name-item-info">Ahmad Bin Ali</span>
+                                <span className="name-item-info">Al Bayt</span>
                             </div>
                             <div className="item-info">
                                 <ChanelBackground />
-                                <span className="name-item-info">Beinsport 02</span>
+                                <span className="name-item-info">Beinsport 04</span>
                             </div>
                             <div className="item-info">
                                 <CommentatorBackground />
-                                <span className="name-item-info">Raouf khaleef</span>
+                                <span className="name-item-info">Issam Chaouali</span>
                             </div>
                         </div>
-                        <div className="match-play">
-                            <span className="team">
-                                <img src={flagBrazil} alt="Flag Morocco" />
-                                <span className="team-name">Brazil</span>
+                        <div className="match-time">
+                        <div className="teams">
+                            <div className="team">
+                                <img src={flagMorocco} alt="" />
+                                <span className="team-name">Morocco</span>
+                            </div>
+                            <span className="vs">
+                                <Vs />
                             </span>
-                            <button className="play-button" aria-label="Play Button">
-                                <PlayIcon />
-                            </button>
-                            <span className="team team-right">
-                                <img src={flagSwitzerland} alt="Flag Morocco" />
-                                <span className="team-name">Switzerland</span>
-                            </span>
+                            <div className="team">
+                                <img src={flagMorocco} alt="" />
+                                <span className="team-name">Morocco</span>
+                            </div>
                         </div>
+                        <div className="start-time">
+                            <Clock />
+                            <div className="time">
+                                <span>17</span>
+                                <span>:</span>
+                                <span>00</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="match">
                     <div className="match-info">
-                            <div className="item-info">
-                                <LocatoinBackground />
-                                <span className="name-item-info">Al Janoub</span>
+                        <div className="item-info">
+                            <LocatoinBackground />
+                            <span className="name-item-info">Lusail</span>
+                        </div>
+                        <div className="item-info">
+                            <ChanelBackground />
+                            <span className="name-item-info">Beinsport 05</span>
+                        </div>
+                        <div className="item-info">
+                            <CommentatorBackground />
+                            <span className="name-item-info">Hafid Derradji</span>
+                        </div>
+                    </div>
+                    <div className="match-time">
+                        <div className="teams">
+                            <div className="team">
+                                <img src={flagMorocco} alt="" />
+                                <span className="team-name">Morocco</span>
                             </div>
-                            <div className="item-info">
-                                <ChanelBackground />
-                                <span className="name-item-info">Beinsport 01</span>
-                            </div>
-                            <div className="item-info">
-                                <CommentatorBackground />
-                                <span className="name-item-info">Yousef Saif</span>
+                            <span className="vs">
+                                <Vs />
+                            </span>
+                            <div className="team">
+                                <img src={flagMorocco} alt="" />
+                                <span className="team-name">Camerooonooon</span>
                             </div>
                         </div>
-                        <div className="match-play">
-                            <span className="team">
-                                <img src={flagSerbia} alt="Flag Morocco" />
-                                <span className="team-name">Serbia</span>
-                            </span>
-                            <button className="play-button" aria-label="Play Button">
-                                <PlayIcon />
-                            </button>
-                            <span className="team team-right">
-                                <img src={flagCameroon} alt="Flag Morocco" />
-                                <span className="team-name">Cameroon</span>
-                            </span>
+                        <div className="start-time">
+                            <Clock />
+                            <div className="time">
+                                <span>17</span>
+                                <span>:</span>
+                                <span>00</span>
+                            </div>
                         </div>
+                    </div>
                 </div>
             </div>
-        </section> 
+        </section>
     </main>
 )
 
