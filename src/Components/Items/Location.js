03f2b247f56edc6a70f90ed39121cfa1c7852de7
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-10-22 13:40:43 +0100

    merge desktop

--- src/Components/Items/Location.js
+++ src/Components/Items/Location.js
@@ -1,11 +1,14 @@
 const LocatoinBackground = () => (
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 182.832 41.404">
-  <g id="Group_884" data-name="Group 884" transform="translate(-582 -309.627)">
-    <path id="Path_1811" data-name="Path 1811" d="M379.1,766.069H531.971l5.98,16.69-5.98,16.69H379.1l-5.98-16.69Z" transform="translate(226.88 -452.327)" fill="#e32052"/>
-    <rect id="Rectangle_176" data-name="Rectangle 176" width="28.415" height="30.139" transform="matrix(-0.707, 0.707, -0.707, -0.707, 623.404, 330.939)" fill="#47b7ab"/>
-    <path id="Path_30" data-name="Path 30" d="M89.068,30.947a5.7,5.7,0,0,0-5.519,5.842c0,4.576,5,9.3,5.213,9.5a.444.444,0,0,0,.613,0c.212-.2,5.213-4.925,5.213-9.5A5.7,5.7,0,0,0,89.068,30.947Zm0,9.022a3.226,3.226,0,1,1,3.066-3.222A3.148,3.148,0,0,1,89.068,39.969Z" transform="translate(514.225 291.711)" fill="#fff"/>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.756 43.646">
+  <g id="Group_884" data-name="Group 884" transform="translate(-582.006 -309.631)">
+    <path id="Path_1811" data-name="Path 1811" d="M379.425,766.069H540.6l6.3,17.6-6.3,17.6H379.425l-6.3-17.6Z" transform="translate(227.857 -452.104)" fill="#e32052"/>
+    <rect id="Rectangle_176" data-name="Rectangle 176" width="29.959" height="31.776" transform="translate(625.653 332.096) rotate(135)" fill="#47b7ab"/>
+    <path id="Path_30" data-name="Path 30" d="M89.368,30.947a6.01,6.01,0,0,0-5.819,6.159c0,4.825,5.272,9.805,5.5,10.016a.468.468,0,0,0,.646,0c.224-.211,5.5-5.193,5.5-10.016a6.01,6.01,0,0,0-5.82-6.159Zm0,9.512a3.4,3.4,0,1,1,3.233-3.4A3.319,3.319,0,0,1,89.368,40.459Z" transform="translate(515.081 292.419)" fill="#fff"/>
   </g>
 </svg>
+
+
+
 )
 
 export default LocatoinBackground
