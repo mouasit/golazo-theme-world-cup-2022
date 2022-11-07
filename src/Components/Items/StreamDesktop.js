62cb3f59785fb297cbc09c907cbff9a50c43021a
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-11-08 00:54:23 +0100

    add stats desktop

--- src/Components/Items/StreamDesktop.js
+++ src/Components/Items/StreamDesktop.js
@@ -1,11 +1,16 @@
 import MatchInfo from "./MatchInfo";
+import Stats from "./Stats";
+import Video from "../../assets/morocco.mp4";
+
 
 const StreamMatchDesktop = (props) => {
     return (
         <div className="stream-desktop">
             <div className="stream-gam-plan">
-                <MatchInfo lang = {props.lang}  />
+                <MatchInfo lang={props.lang}/>
+                <video src= {Video} controls></video>
             </div>
+            <Stats displayStats="view-stats show-list" />
         </div>
     )
 }
