1ddedc1bad1eb331da916d4febfce63f0a71c43f
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-11-08 02:39:25 +0100

    add gam plan desktop

--- src/Components/Items/StreamDesktop.js
+++ src/Components/Items/StreamDesktop.js
@@ -1,14 +1,18 @@
 import MatchInfo from "./MatchInfo";
 import Stats from "./Stats";
 import Video from "../../assets/morocco.mp4";
+import Gamplan from "./GamPlan";
 
 
 const StreamMatchDesktop = (props) => {
     return (
         <div className="stream-desktop">
             <div className="stream-gam-plan">
-                <MatchInfo lang={props.lang}/>
-                <video src= {Video} controls></video>
+                <div className="stream">
+                    <MatchInfo lang={props.lang}/>
+                    <video src= {Video} controls></video>
+                </div>
+                <Gamplan displayGamPlan = "view-gam-plan show-list" />
             </div>
             <Stats displayStats="view-stats show-list" />
         </div>
