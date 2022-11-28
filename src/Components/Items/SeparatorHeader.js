b359d1a7d75b10588c61826df90a4592fd2b01a3
MustaphaOuassit <mustaphaouassit60@gmail.com>
2022-11-28 14:13:26 +0100

    add header of card ranking

---
+++ src/Components/Items/SeparatorHeader.js
@@ -0,0 +1,12 @@
+const SeparatorHeader = () => (
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 309.186 18.774">
+  <g id="Group_924" data-name="Group 924" transform="translate(-1023.935 -692.548)">
+    <rect id="Rectangle_184" data-name="Rectangle 184" width="299.799" height="1.46" transform="translate(1023.935 701.205)" fill="#d2a762"/>
+    <path id="Path_1823" data-name="Path 1823" d="M0,0,12.833.442l.442,12.833L.442,12.833Z" transform="matrix(-0.707, -0.707, 0.707, -0.707, 1323.734, 711.322)" fill="#d2a762"/>
+  </g>
+</svg>
+
+);
+
+export default SeparatorHeader;
--- src/Components/Ranking/Ranking.js
+++ src/Components/Ranking/Ranking.js
@@ -1,6 +1,44 @@
+import CardRanking from "../Items/CardRanking";
+import SeparatorRanking from "../Items/SeparatorRanking";
+import SeparatorHeader from "../Items/SeparatorHeader";
+
 const Ranking = () =>(
     <main>
-        <h1>Ranking</h1>
+        <div className="ranking">
+        <section className="ranking-teams">
+            <div className="row-cards">
+                <div className="cards">
+                    <CardRanking />
+                    <div className="info-ranking">
+                        <div className="header-ranking">
+                            <span className="title-group">
+                                Group A
+                            </span>
+                            <span className="details-ranking">
+                                <span>P</span>
+                                <span>W</span>
+                                <span>D</span>
+                                <span>L</span>
+                                <span>PTS</span>
+                            </span>
+                        </div>
+                        <span className="separator-header">
+                            <SeparatorHeader />
+                        </span>
+                        <div className="body-ranking">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="separator-ranking">
+                    <SeparatorRanking />
+                </div>
+            </div>
+        </section>
+        <section className="ranking-scorers">
+
+        </section>
+        </div>
     </main>
 )
 
